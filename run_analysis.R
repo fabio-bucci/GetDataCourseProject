86515c96b2ce897a83c67bfbb5e4ee257525b328
Fabio Bucci <bucci.fabio@gmail.com>
2015-02-15 17:24:44 +0100

    Added columns subsetting for  mean and std variables

--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,25 @@
-# This function prepare test and train data sets, reading the data file, reading the subject file, reading the activity file and 
-# properly nameing all variables with descriptive name
+## This function prepare test and train data sets, reading the data file, reading the subject file, reading the activity file and 
+## properly nameing all variables with descriptive name
 prepareDataset <- function(type,columnNames,activityLabels){
     if(type != 'train' && type != 'test') {
         stop("Expected train or test dataset")
     }
-    ## Read the data and appropriately labels the data set with descriptive variable names.
+    # Read the data and appropriately labels the data set with descriptive variable names.
     data <- read.table(paste('./UCI HAR Dataset/',type,'/X_',type,'.txt', sep = ""), header = FALSE)
+    # Set the variable names
     names(data) <- columnNames$colName
-    ## Read subjects file
+    # Read subjects file
     subjects <- read.table(paste('./UCI HAR Dataset/',type,'/subject_',type,'.txt', sep = ""), header = FALSE, col.names = "subject")
-    ## Uses descriptive activity names to name the activities in the data set
-    activities <- read.table(paste('./UCI HAR Dataset/',type,'/y_',type,'.txt', sep = ""), header = FALSE,col.names = "activityNum")
-    activities$activityNum <- as.factor(activities$activityNum)
-    levels(activities$activityNum) <- activityLabels$activityName
-    ## Merge data, subjects and activities
+    # Uses descriptive activity names to name the activities in the data set
+    activities <- read.table(paste('./UCI HAR Dataset/',type,'/y_',type,'.txt', sep = ""), header = FALSE,col.names = "activity")
+    activities$activity <- as.factor(activities$activity)
+    levels(activities$activity) <- activityLabels$activityName
+    # Merge data, subjects and activities
     data <- cbind(data, subjects, activities)
     data
 }
 
-
-
-
-### If data folder is not present, download and unzip the source file
+## If data folder is not present, download and unzip the source file
 if(!file.exists("./UCI HAR Dataset")){
     print("Downloading files...")
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
@@ -30,24 +28,26 @@ if(!file.exists("./UCI HAR Dataset")){
     unzip(destinationZipFile)
     file.remove(destinationZipFile)
 }
-## Read activity labels
-activityLabels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE, col.names=c("activityNum","activityName"))
-## Read the "features.txt" file and store column names
+# Read activity labels
+activityLabels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE, col.names=c("activity","activityName"))
+# Read the "features.txt" file and store column names
 columnNames <- read.table('./UCI HAR Dataset/features.txt', header = FALSE, col.names=c("colNum","colName"))
 
-## Read the test data
+# Read the test data
 testData <- prepareDataset('test',columnNames,activityLabels)
-## Read the training data
+# Read the training data
 trainData <- prepareDataset('train',columnNames,activityLabels)
-## Merges the training and the test sets to create one data set.
+# Merges the training and the test sets to create one data set.
 allData <- rbind(trainData,testData)
 
-### 2. Extracts only the measurements on the mean and standard deviation for 
+### Extracts only the measurements on the mean and standard deviation for 
 ### each measurement. 
-# grep("*-mean\\(\\)*|*-std\\(\\)*",names(allData), value = TRUE)
 
-### 4. Appropriately labels the data set with descriptive variable names. 
-
+# Select all columns representing mena and standard deviation values based on 
+# the dataset variables naming convention, plus the activity and subject columns
+columnSelection <- c(grep("*-mean\\(\\)*|*-std\\(\\)*",names(allData), value = TRUE),"activity","subject")
+allDataMeanStd <- allData[,names(allData) %in% columnSelection]
+ 
 ### From the data set in step 4, creates a second, independent tidy data set with 
 ### the average of each variable for each activity and each subject.
 
